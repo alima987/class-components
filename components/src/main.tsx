d25d9d9d10fa4c52a66d2bf9d64f53de9056cf7a
alima987 <alima1996b@gmail.com>
2024-07-05 11:01:00 +0500

    feat: add router and 404 page

--- components/src/main.tsx
+++ components/src/main.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  }
+])
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
