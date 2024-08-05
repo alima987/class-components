4faee8937a5a3b742670be51757e2b00f791b827
alima987 <alima1996b@gmail.com>
2024-08-05 21:37:11 +0500

    feat: add slider component

--- components/src/components/Slider/Slider.tsx
+++ components/src/components/Slider/Slider.tsx
@@ -1,3 +1,77 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from './Slider.module.css';
+import { MdArrowForwardIos, MdArrowBackIos } from "react-icons/md";
+import { MovieData } from "../../redux/slices/movieSlice";
+
+interface SliderProps {
+  items: MovieData[];
+  children: React.ReactNode;
+}
+
+const Slider = ({ items, children}: SliderProps) => {
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [distance, setDistance] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [totalInViewport, setTotalInViewport] = useState(0);
+  const [viewed, setViewed] = useState(0);
+  
+  const listRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    if (listRef.current && items.length) {
+      const containerWidth = listRef.current.clientWidth;
+      const itemWidth = (listRef.current.firstElementChild as HTMLElement)?.clientWidth || 0;
+      const totalInViewport = Math.ceil(containerWidth / itemWidth);
+
+      setContainerWidth(containerWidth);
+      setTotalInViewport(totalInViewport);
+    }
+  }, [items]);
+
+  const handleClick = (direction: "left" | "right") => {
+    setIsMoved(true);
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      setViewed(viewed - totalInViewport);
+      setDistance(distance + containerWidth);
+      if (listRef.current) {
+        listRef.current.style.transform = `translate3d(${distance + containerWidth}px, 0, 0)`;
+      }
+    }
+    if (direction === "right" && (viewed + totalInViewport) < items.length) {
+      setSlideNumber(slideNumber + 1);
+      setViewed(viewed + totalInViewport);
+      setDistance(distance - containerWidth);
+      if (listRef.current) {
+        listRef.current.style.transform = `translate3d(${distance - containerWidth}px, 0, 0)`;
+      }
+    }
+  };
+
+  return (
+    <div className={styles.top_cont}>
+      <div className={styles.top_wrapper}>
+        <MdArrowBackIos 
+          className={`${styles.sliderArrow} ${styles.left}`}
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved ? "none" : "block" }}
+        />
+        <div className={styles.toprated_list} ref={listRef}>
+        {children}
+        </div>
+        <MdArrowForwardIos 
+          className={`${styles.sliderArrow} ${styles.right}`}
+          onClick={() => handleClick("right")}
+          style={{ display: slideNumber >= Math.floor(items.length / totalInViewport) ? "none" : "block" }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Slider;
 
 
 
--- components/src/components/TopRated/TopRated.tsx
+++ components/src/components/TopRated/TopRated.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useRef, useState } from "react"
+import React, { useEffect } from "react"
 import styles from './TopRated.module.css'
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
@@ -7,71 +7,30 @@ import { setTopRated } from "../../redux/slices/movieSlice";
 import { useFetchTopRatedQuery } from '../../services/movieApi'
 import { setLoading } from "../../redux/slices/lodingSlice";
 import Link from "next/link";
-import { MdArrowForwardIos, MdArrowBackIos } from "react-icons/md";
+import Slider from '../Slider/Slider';
 
 const TopRated = () => {
   const movies = useSelector((state: RootState) => state.movies.topRated); 
   const isLoading = useSelector((state: RootState) => state.loading.isLoading);
   const dispatch = useDispatch();
   const { data, error, isLoading: queryIsLoading } = useFetchTopRatedQuery({});
-  const [isMoved, setIsMoved] = useState(false);
-  const [slideNumber, setSlideNumber] = useState(0);
-  const [distance, setDistance] = useState(0);
-  const [containerWidth, setContainerWidth] = useState(0);
-  const [totalInViewport, setTotalInViewport] = useState(0);
-  const [viewed, setViewed] = useState(0);
-  
-  const listRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     dispatch(setLoading(queryIsLoading));
     if (data) {
       dispatch(setTopRated(data.results));
     }
-
-    if (listRef.current && movies.length) {
-      const containerWidth = listRef.current.clientWidth;
-      const itemWidth = (listRef.current.firstElementChild as HTMLElement)?.clientWidth || 0;
-      const totalInViewport = Math.ceil(containerWidth / itemWidth);
-
-      setContainerWidth(containerWidth);
-      setTotalInViewport(totalInViewport);
-    }
-  }, [data, queryIsLoading, dispatch, movies]);
+  }, [data, queryIsLoading, dispatch]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Failed to load top-rated movies.</div>;
-
-  const handleClick = (direction: "left" | "right") => {
-    setIsMoved(true);
-    if (direction === "left" && slideNumber > 0) {
-      setSlideNumber(slideNumber - 1);
-      setViewed(viewed - totalInViewport);
-      setDistance(distance + containerWidth);
-      if (listRef.current) {
-        listRef.current.style.transform = `translate3d(${distance + containerWidth}px, 0, 0)`;
-      }
-    }
-    if (direction === "right" && (viewed + totalInViewport) < movies.length) {
-      setSlideNumber(slideNumber + 1);
-      setViewed(viewed + totalInViewport);
-      setDistance(distance - containerWidth);
-      if (listRef.current) {
-        listRef.current.style.transform = `translate3d(${distance - containerWidth}px, 0, 0)`;
-      }
-    }
-  };
 
   return (
     <div className={styles.top_cont}>
       <h2 className={styles.top_title}>Top Rated Movies</h2>
       <div className={styles.top_wrapper}>
-        <MdArrowBackIos 
-          className={`${styles.sliderArrow} ${styles.left}`}
-          onClick={() => handleClick("left")}
-          style={{ display: !isMoved ? "none" : "block" }}
-        />
-        <div className={styles.toprated_list} ref={listRef}>
+      <Slider items={movies}>
+      <div className={styles.toprated_list}>
           {movies.map((el) => (
             <div key={el.id} className={styles.toprated_item}>
               <Link href={`/detail/${el.id}`}>
@@ -82,11 +41,7 @@ const TopRated = () => {
             </div>
           ))}
         </div>
-        <MdArrowForwardIos 
-          className={`${styles.sliderArrow} ${styles.right}`}
-          onClick={() => handleClick("right")}
-          style={{ display: slideNumber >= Math.floor(movies.length / totalInViewport) ? "none" : "block" }}
-        />
+        </Slider>
       </div>
     </div>
   );
