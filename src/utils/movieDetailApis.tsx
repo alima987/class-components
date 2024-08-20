31d4fe832c524c8585b01db7fc854c93b8c964d0
alima987 <alima1996b@gmail.com>
2024-08-20 11:25:11 +0500

    refactor: fix adaptive styles

--- src/utils/movieDetailApis.tsx
+++ src/utils/movieDetailApis.tsx
@@ -6,14 +6,20 @@ export interface DetailProps {
     }
   }
   export async function getMovieDetail( id: DetailData['id']) {
-    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=51ca1e241e720d72e2bb92a4b36859f5`);
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=51ca1e241e720d72e2bb92a4b36859f5`, {
+      next: { revalidate: 10 }, 
+    });
     return res.json();
   }
   export async function getCastDetail( id: DetailData['id']) {
-    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?language=en-US&api_key=51ca1e241e720d72e2bb92a4b36859f5`);
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits?language=en-US&api_key=51ca1e241e720d72e2bb92a4b36859f5`, {
+      next: { revalidate: 10 }, 
+    });
     return res.json();
   }
   export async function getReviewDetail( id: DetailData['id']) {
-    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?language=en-US&api_key=51ca1e241e720d72e2bb92a4b36859f5`);
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?language=en-US&api_key=51ca1e241e720d72e2bb92a4b36859f5`, {
+      next: { revalidate: 10 }, 
+    });
     return res.json();
   }
